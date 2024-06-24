f1fc0640bd4d76421b609999e92e80ab3d2f849a
Erick Jhair Lozano Gomez <132631485+ErickLG281@users.noreply.github.com>
2024-06-23 22:27:19 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,38 @@
+export const videos = [
+  {
+    "titulo": "Arcane 2",
+    "linkVideo": "https://youtu.be/g-6g2uEjF1s?si=X2ORaciuEMAgtCiR",
+    "linkImage": "https://i.postimg.cc/BnZ7TFbL/images.jpg",
+    "categoria": "Anime",
+    "descripcion": "Segunda Temporda",
+    "codigoSeguridad": "123",
+    "id": 2
+    },
+    {
+    "titulo": "Frieren: Beyond Journey's End",
+    "linkVideo": "https://youtu.be/vEF077uVx8M?si=s8w_HgeJbYSBLQ97",
+    "linkImage": "https://i.postimg.cc/26PQ820W/Frieren-M-s-All-Del-Final-Del-Viaje-Poster.webp",
+    "categoria": "Anime",
+    "descripcion": "Mas Alla del Final",
+    "codigoSeguridad": "100",
+    "id": 4
+    }
+      
+]
+
+export const categorias = [
+  {
+    id: '1',
+    nombre: 'Anime',
+    descripcion: 'Anime en general',
+    color: '#005792',
+    codigoSeguridad: 'Anime21',
+  },
+  {
+    id: '2',
+    nombre: 'Acción',
+    descripcion: 'Peliculas de Acción',
+    color: '#00C86F',
+    codigoSeguridad: 'Acc21',
+  }
+]
